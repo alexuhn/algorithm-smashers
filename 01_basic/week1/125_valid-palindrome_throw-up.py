706a47c670cbd768fa7bf14fe59fb20c518c6bd9
Lazyperfection <110768442+Lazyperfection@users.noreply.github.com>
2022-08-12 12:08:48 +0900

    throw-up

--- 01_basic/week1/125_valid-palindrome_throw-up.py
+++ 01_basic/week1/125_valid-palindrome_throw-up.py
@@ -1,1 +1,19 @@
-asdsdas
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+       
+        strings = []
+        for char in s:
+            if char.isalnum():
+                strings.append(char.lower())
+       
+        length = len(strings)
+        
+        for i in range(length):
+            cnt = length - 1 - i
+            if strings[cnt] != strings[i]:
+                return False
+        return True
+                
+                
+        
+            
