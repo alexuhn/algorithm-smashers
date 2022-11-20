21136fbb535e8ce12c5a2932a101aec541f67886
Sam Juhyun Kang <55772399+kjh91-sam@users.noreply.github.com>
2022-11-20 19:44:11 +0900

    🦭Add_Solution_trapping_rain_water.py (add another solution)

--- 02_rookie/week7/42_trapping-rain-water_seal.py
+++ 02_rookie/week7/42_trapping-rain-water_seal.py
@@ -21,3 +21,37 @@ class Solution:
             rain[i] = cur
 
         return sum([rain[i] - height[i] for i in range(len(height))])
+
+# 제일 빠르다는거 같이보려고 가져옴
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """
+        Idea: Go one way and cast shadows. Then go the other way.
+        Runtime O(n)
+        """
+
+        tallest = height[0]
+        area = 0
+        tallest_index = 0
+        running_area = 0
+        for i in range(1, len(height)):
+            curr = height[i]
+            if curr >= tallest:
+                tallest = curr
+                area += running_area
+                tallest_index = i
+                running_area = 0
+            else:
+                running_area += tallest - curr
+
+        tallest = height[-1]
+        running_area = 0
+        for i in range(len(height) - 1, tallest_index - 1, -1):
+            curr = height[i]
+            if curr >= tallest:
+                tallest = curr
+                area += running_area
+                running_area = 0
+            else:
+                running_area += tallest - curr
+        return area
