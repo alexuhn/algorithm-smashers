c911889e5a1c9b76b58891a3018ebb4bdcf3bb15
alexuhn <alexuhyun@gmail.com>
2022-12-03 15:53:07 +0900

    🍉 Add another Python solution

--- 02_rookie/week8/15_3sum_watermelon.py
+++ 02_rookie/week8/15_3sum_watermelon.py
@@ -1,3 +1,4 @@
+# Solution 1: 첫 번째 답안
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # three sum 문제 => 하나의 target + two sum 문제
@@ -31,3 +32,39 @@ class Solution:
     
         # 튜플로 구성된 set을 출력 형태에 맞게 리스트로 변경
         return list(result_set)
+
+# Solution 2: 정답을 본 뒤 개선한 답안
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # 투 포인터를 이용하기 위해 nums를 정렬
+        nums.sort()
+        
+        result = []        
+        for i in range(len(nums) - 2):
+            if 0 < i and nums[i] == nums[i - 1]: 
+                continue
+                
+            # 기준이 되는 숫자가 0을 초과하는 경우 더 이상 탐색하지 않아도 된다.
+            if 0 < nums[i]: 
+                break
+                
+            # 투 포인터
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                target = nums[i] + nums[left] + nums[right]
+                
+                if 0 < target:
+                    right -= 1
+                elif target < 0:
+                    left += 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    
+                    # while 문 내에서 하나의 while 문만 도는 것이 더 짧게 걸린다.
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    left += 1
+                    right -= 1
+    
+        return result
+    
