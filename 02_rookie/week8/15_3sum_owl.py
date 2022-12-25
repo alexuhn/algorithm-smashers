54a9184e8d554d07b15b823cec97aa337772c9e1
CheolminKwak <case.steelmin@gmail.com>
2022-12-25 16:43:10 +0900

    week8 fix typo

--- 02_rookie/week8/15_3sum_owl.py
+++ 02_rookie/week8/15_3sum_owl.py
@@ -18,7 +18,7 @@ class Solution:
             for j in range(i + 1, len(nums) - 1):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                for k in range(j + i, len(nums)):
+                for k in range(j + 1, len(nums)):
                     if k > j + 1 and nums[k] == nums[k - 1]:
                         continue
                     if nums[i] + nums[j] + nums[k] == 0:
