0e76b59a784eebf5dd7389ac537f6f72ae781906
Sam Juhyun Kang <55772399+kjh91-sam@users.noreply.github.com>
2023-02-12 18:53:34 +0900

    🦭 Add best-time-to-buy-and-sell-stock solution

---
+++ 02_rookie/week11/121_best-time-to-buy-and-sell-stock_seal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        bottom = prices[0]
+        biggest = 0
+
+        i = 1
+        while (i < len(prices)):
+            biggest = max(biggest, prices[i] - bottom)
+            bottom = min(bottom, prices[i])
+
+            i += 1
+
+        return biggest
