c19ddf5aafef9893ce11808c6ca25c0e3e2bdfa7
Sam Juhyun Kang <55772399+kjh91-sam@users.noreply.github.com>
2023-02-12 18:58:31 +0900

    🦭 Add a faster solution for stock 어쩌고

--- 02_rookie/week11/121_best-time-to-buy-and-sell-stock_seal.py
+++ 02_rookie/week11/121_best-time-to-buy-and-sell-stock_seal.py
@@ -11,3 +11,20 @@ class Solution:
             i += 1
 
         return biggest
+
+
+# second try
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        bottom = prices[0]
+        biggest = 0
+
+        i = 1
+        while (i < len(prices)):
+            if biggest < prices[i] - bottom:
+                biggest = prices[i] - bottom 
+            if bottom > prices[i]:
+                bottom = prices[i] 
+            i += 1
+
+        return biggest
