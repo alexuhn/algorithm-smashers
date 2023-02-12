7fd057128a6225cb310d9dc1306a07e69e802c27
CheolminKwak <case.steelmin@gmail.com>
2023-02-12 18:46:45 +0900

    🦉Add_Python_Solution

---
+++ 02_rookie/week11/121_best-time-to-buy-and-sell-stock_owl.py
@@ -0,0 +1,13 @@
+ mini = prices[0]
+        maxi = prices[1]
+
+        for i in range(0,len(prices)-1):
+            if mini > prices[i]:
+                mini = prices[i]
+            if maxi < prices[i+1]:
+                maxi = prices[i+1]
+            print(mini,maxi)
+        if maxi - mini > 0 and maxi != prices[1]:
+            return maxi - mini
+        elif maxi - mini <= 0 or maxi == prices[1]:
+            return 0
