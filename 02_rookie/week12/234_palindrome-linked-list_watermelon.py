b7b23ad2cab62e13ddf4f928511a17364859baa0
alexuhn <alexuhyun@gmail.com>
2023-02-17 20:59:25 +0900

    🍉 Add another Python solution

--- 02_rookie/week12/234_palindrome-linked-list_watermelon.py
+++ 02_rookie/week12/234_palindrome-linked-list_watermelon.py
@@ -1,3 +1,4 @@
+# ===== Solution 1 =====
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -11,3 +12,32 @@ class Solution:
             head = head.next
 
         return entire_list == entire_list[::-1]
+
+# ===== Solution 2 =====
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head.next == None:
+            return True
+
+        slow = head
+        fast = head
+        reverse = None
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            reverse, reverse.next, slow = slow, reverse, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while slow:
+            if slow.val != reverse.val:
+                return False
+            slow = slow.next
+            reverse = reverse.next
+        return True
