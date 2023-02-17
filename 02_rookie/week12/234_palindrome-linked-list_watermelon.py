c82fa5910ac3141cd031b4378e2bf7e625b257f9
alexuhn <alexuhyun@gmail.com>
2023-02-17 21:13:53 +0900

    🍉 Add another Python solution

--- 02_rookie/week12/234_palindrome-linked-list_watermelon.py
+++ 02_rookie/week12/234_palindrome-linked-list_watermelon.py
@@ -1,4 +1,4 @@
-# ===== Solution 1 =====
+# ===== Solution 1 (792 ms) =====
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -13,7 +13,8 @@ class Solution:
 
         return entire_list == entire_list[::-1]
 
-# ===== Solution 2 =====
+    
+# ===== Solution 2 (665 ms) =====
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -31,6 +32,40 @@ class Solution:
         while fast and fast.next:
             fast = fast.next.next
             reverse, reverse.next, slow = slow, reverse, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while slow:
+            if slow.val != reverse.val:
+                return False
+            slow = slow.next
+            reverse = reverse.next
+        return True
+
+    
+# ===== Solution 3 (674 ms) =====
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head.next == None:
+            return True
+
+        slow = head
+        fast = head
+        reverse = None
+        
+        while fast and fast.next:
+            fast = fast.next.next       # fast 이동
+            prev_reverse = reverse      # reverse 저장
+            next_slow = slow.next       # slow.next 저장
+            reverse = slow              # reverse 변경
+            reverse.next = prev_reverse # reverse를 역으로 연결
+            slow = next_slow            # slow 이동
 
         if fast:
             slow = slow.next
